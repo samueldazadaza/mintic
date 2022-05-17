a120de3433d29a6d72ef718a355b0bd6ad280b72
sam <samueldazadaza@gmail.com>
2022-05-16 19:21:51 -0500

    crear lista prueba1

---
+++ S4/listas.py
@@ -0,0 +1,4 @@
+# Una lista es una colección de datos que se utiliza para guardar varios elementos
+# Una lista es indexada de forma numerica
+# Una lista es indexada de forma numérica
+# Las listas son mutables
