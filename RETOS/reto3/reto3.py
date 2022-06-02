1f9a777de05d1288da9b3dcf10dcd0c6738550f4
sam <samueldazadaza@gmail.com>
2022-06-02 10:38:19 -0500

    comenzando semana 5

--- RETOS/reto3/reto3.py
+++ RETOS/reto3/reto3.py
@@ -11,22 +11,22 @@ def Autopartes(ventas: list):
         diccionario = dict(zip(range(len(ventas)), ventas))
         return diccionario
 
-def consultaRegistro(ventas, idPoducto):
+def consultaRegistro(ventas: dict, idPoducto):
         #para preguntar en ventas si existe el producto
         for i in ventas:
                 if ventas[i][0] == idProducto:
                         dProducto = ventas[i][1]
-                        pnProducto = ventas [i][2]
-                        cvProducto = ventas [i][3]
-                        sProducto = ventas [i][4]
-                        nComprador = ventas [i][5]
-                        cComprador = ventas [i][6]
-                        fVenta = ventas [i][7]
-                        respuesta = f"Producto consultado : {idProducto} Descripción {dProducto} Parte {pnProducto} Cantidad vendida {cvProducto} Stock {sProducto} Comprador {nComprador} Documento {cComprador} Fecha Venta {fVenta}"
+                        pnProducto = ventas[i][2]
+                        cvProducto = ventas[i][3]
+                        sProducto = ventas[i][4]
+                        nComprador = ventas[i][5]
+                        cComprador = ventas[i][6]
+                        fVenta = ventas[i][7]
+                        print(f"Producto consultado : {idProducto} Descripción {dProducto} Parte {pnProducto} Cantidad vendida {cvProducto} Stock {sProducto} Comprador {nComprador} Documento {cComprador} Fecha Venta {fVenta}")
+        return f""
                 # else:
                 #         print("No hay registro de venta de ese producto")
                 #print(ventas)
-        return respuesta
 
 ventas = [
 	(2001,'rosca', 'PT29872',2,45,'Luis Molero',3456,'12/06/2020'),
@@ -37,3 +37,13 @@ ventas = [
 ]
 
 print(consultaRegistro(Autopartes(ventas),2010))
+
+#DE AQUI PARA ABAJO ES UNA PRUEBA ANTIGUA!!
+"""
+                        respuesta = f"Producto consultado : {idProducto} Descripción {dProducto} Parte {pnProducto} Cantidad vendida {cvProducto} Stock {sProducto} Comprador {nComprador} Documento {cComprador} Fecha Venta {fVenta}"
+                # else:
+                #         print("No hay registro de venta de ese producto")
+                #print(ventas)
+        return respuesta
+
+"""
