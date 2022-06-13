c44dc3b58be452c1f03bdcd8f7a18cb7f50bfbd9
sam <samueldazadaza@gmail.com>
2022-06-13 05:26:49 -0500

    reto 4 ok 😎😊

--- 1-PROGRAMACION/G4/RETOS/RETO4/reto4.py
+++ 1-PROGRAMACION/G4/RETOS/RETO4/reto4.py
@@ -1,19 +1,19 @@
 def ordenes(rutinaContable):
     from functools import reduce
-    total = list(map(lambda x: [x[0]]+list(map(lambda y:y[1]*y[2] ,x[1:])),rutinaContable))
-    print(total)
-    total = list(map(lambda x: [x[0]]+[reduce(lambda a,b: round(a+b, 2) ,x[1:])],total))
-    print(total)
-    total = list(map(lambda x: x if x[1]>=600000 else [] ,total))
+    total = list(map(lambda x: [x[0]]+list(map(lambda y:y[1]*y[2] ,x[1:])),rutinaContable)) #para recorrer lista
+    total = list(map(lambda x: [x[0]]+[reduce(lambda a,b: round(a+b, 2) ,x[1:])],total)) #para redondear decimal a 2 cifras
+    total = list(map(lambda x: x if x[1]>=600000 else [x[0], x[1]+25000] ,total)) #para aumentar los 25mil
     
-    print('----------------- Inicio Registro diario --------------------------')
-    print('----------------- Fin Registro diario -----------------------------')
+    print('------------------------ Inicio Registro diario ---------------------------------')
+    for i in range(len(total)):
+        print(f'La factura {total[i][0]} tiene un total en pesos de {total[i][1]:,.2f}')
+    print('-------------------------- Fin Registro diario ----------------------------------')
+    
+#copiar codigo en robot imaster de aqui hacia arriba
 
 rutinaContable = [
-    [1201, ("5464", 4, 25842.99), ("7854",18,23254.99), ("8521", 9, 48951.95)],
-    [1202, ("8756", 3, 115362.58),("1112",18,2354.99)],
-    [1203, ("2547", 1, 125698.20), ("2635", 2, 135645.20), ("1254", 1, 13645.20),("9965", 5, 1645.20)],
-    [1204, ("9635", 7, 11.99), ("7733",11,18.99), ("88112", 5, 390.95)]
+    [6587, ("3268", 4, 25842.99), ("8274",18,23254.99), ("6548", 9, 48951.95), ("2547", 5, 8951.95)],
+    [6588, ("1254", 3, 115362.58), ("9744", 2, 99235.66)],
 ]
 
 ordenes(rutinaContable)
