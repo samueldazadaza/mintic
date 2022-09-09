e098223b927b994e70650e6b92d3f3b06109a8ef
sam <samueldazadaza@gmail.com>
2022-09-08 20:05:12 -0500

    min 01:30:00 clase 290622

---
+++ CICLO2/1 PROGRAMACION/S2-C2/Semana2/src/Calcular.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calcular {
+    public void carturaDatos(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite el valor 1");
+        float valor1 = sc.nextFloat();
+        System.out.println("Digite el valor 2");
+        float valor2 = sc.nextFloat();
+        sc.close();
+        System.out.println(suma(valor1, valor2));
+        System.out.println(resta(valor1, valor2));
+        System.out.println(multi(valor1, valor2));
+        System.out.println(divi(valor1, valor2));
+    }
+    public float suma(float v1, float v2){
+        return v1 + v2;
+    }
+    public float resta(float v1, float v2){
+        return v1 - v2;
+    }
+    public float multi(float v1, float v2){
+        return v1 * v2;
+    }
+    public float divi(float v1, float v2){
+        return v1 / v2;
+    }
+
+    public static void main(String[] args){ //cuando dice void el metodo no retorna ningun dato
+
+    }
+}
