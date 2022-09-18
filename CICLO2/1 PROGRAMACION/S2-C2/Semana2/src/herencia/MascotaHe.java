06f7fd97116f0296d83107848e0f7281ddbabd06
sam <samueldazadaza@gmail.com>
2022-09-17 23:27:05 -0500

    QUEDE en min 01:00:00 del video 060722

---
+++ CICLO2/1 PROGRAMACION/S2-C2/Semana2/src/herencia/MascotaHe.java
@@ -0,0 +1,10 @@
+//Herencia 
+//Clase padre
+public class MascotaHe {
+    protected String nombre;
+    protected String raza;
+    protected int edad;
+    public void dormir(){
+        System.out.println("La mascota duerme ZZZZ");
+    }
+}
